1b00a4069edcf3e5bb57cfc48bc794e22907e5bd
Ebonie Butler <ebonie@yikesinc.com>
2023-07-16 17:51:12 -0400

    epi 43 changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,9 +14,11 @@ class Post extends Model {
 
     public function scopeFilter( $query, $filters ) {
         $query->when( $filters['search'] ?? false,
-            fn( $query, $search ) => $query
-                ->where( 'title', 'like', '%' . $search . '%' )
-                ->orWhere( 'body', 'like', '%' . $search . '%' ) );
+            fn( $query, $search ) => $query->where(
+                fn( $query ) => $query->where( 'title', 'like', '%' . $search . '%' )
+                                      ->orWhere( 'body', 'like', '%' . $search . '%' ) )
+        );
+
 
         $query->when( $filters['category'] ?? false,
             fn( $query, $category ) => $query
